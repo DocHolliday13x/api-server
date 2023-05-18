e8fc170c4ea393792c51b48f2909b6c368735dae
Ryan Eastman <rking1323@gmail.com>
2023-05-17 19:40:19 -0700

    finished models

--- src/models/clothes.js
+++ src/models/clothes.js
@@ -1,1 +1,26 @@
 `use strict`;
+
+module.exports = (sequelize, DataTypes) => sequelize.define('clothes', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  brand: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    values: ['URT', 'Forged', 'Altama', 'Kuhl', 'Arcteryx', 'Patagonia', 'Gatorz'],
+  },
+});
+
+// clothes will be the name of the table created, pluralized
+
+
+
+
+
+
+
--- src/models/food.js
+++ src/models/food.js
@@ -1,1 +1,25 @@
 `use strict`;
+
+module.exports = (sequelize, DataTypes) => sequelize.define('food', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    values: ['FRUIT', 'VEGETABLE', 'PROTEIN', 'GRAIN', 'DAIRY'],
+  },
+});
+
+// food will be the name of the table created
+// each property will be a field/column of the table
+
+
+
+
+
